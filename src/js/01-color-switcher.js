bada278c677214ab62ddd52daa89c12a858ecfa1
vovkatom <36505066+vovkatom@users.noreply.github.com>
2023-09-22 22:00:31 +0300

    new

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let intervalId = false;
+stopButton.disabled = true;
+
+startButton.addEventListener('click', () => {
+  if (!intervalId) {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+    intervalId = null;
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
+});
